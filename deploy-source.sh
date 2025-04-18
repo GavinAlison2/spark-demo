816ea192cfaf3ddb207581b7079cfefccb0cd38b
alice <921757697@qq.com>
2025-04-18 21:01:08 +0800

    deploy spark demo to github

--- deploy-source.sh
+++ deploy-source.sh
@@ -5,12 +5,12 @@ git init .
 
 git add -A
 
-#git rm -r --cached .idea
+git rm -r --cached .idea
 
 git commit -m 'deploy spark demo to github'
 
-git config --global user.name "GavinAlison2"
-git config --global user.email "921757697@qq.com"
+#git config --global user.name "GavinAlison2"
+#git config --global user.email "921757697@qq.com"
 
 #git remote add origin git@github.com:GavinAlison2/spark-demo.git
 git branch -M master
