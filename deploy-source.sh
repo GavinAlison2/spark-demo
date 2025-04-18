2e57c098c75ebffb567939026586c56de4c84582
root <root@DESKTOP-FGLNHVU>
2025-04-18 20:51:31 +0800

    doc 整理

--- deploy-source.sh
+++ deploy-source.sh
@@ -10,10 +10,6 @@ git rm -r --cached .idea
 git commit -m 'doc 整理'
 
 git remote add origin git@github.com:GavinAlison2/spark-demo.git
-
-git pull origin master --allow-unrelated-histories
-# git remote add origin git@github.com:GavinAlison2/simple-doc.git
 git branch -M master
-# git push  -f origin master:gh-pages
-git push -f origin master
+git push -f -u origin master
 
