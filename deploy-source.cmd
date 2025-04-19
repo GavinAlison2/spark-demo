9c9e21148faef23d171e7bd0f2e5b82ed866f0a5
alice <921757697@qq.com>
2025-04-19 10:01:24 +0800

    deploy

--- deploy-source.cmd
+++ deploy-source.cmd
@@ -1,19 +1,10 @@
-@echo off
 
 git config core.autocrlf false
 git rm -r --cached .idea
 git add .
-git commit -m 'deploy spark-sql demo to github'
+git commit -m "deploy"
 ::  git remote add origin git@github.com:GavinAlison2/spark-demo.git
 :: git branch -M master
 :: git push -f -u origin master
 git push -f origin master
 
-:: 检查上一条命令的执行结果
-if %errorlevel% neq 0 (
-    echo 推送代码到远程仓库失败，请检查错误信息。
-    exit /b %errorlevel%
-)
-
-:: 如果需要，可以在这里添加更多的错误处理或成功信息
-echo 代码已成功推送到远程仓库。
